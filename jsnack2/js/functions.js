6fbe3adf53aaec32ba0e487882dad84800056f66
Denis-Turbatu <turbatudenis81@gmail.com>
2024-04-15 16:48:51 +0200

    feat: jsnack2 completed

--- jsnack2/js/functions.js
+++ jsnack2/js/functions.js
@@ -2,4 +2,4 @@
 
 function generatorePuntiFalli(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -34,9 +34,34 @@ const squadreCalcio = [
 
 // Ciclo forEach per salvare ad ogni iterazione i punti e i falli per ogni squadra
 squadreCalcio.forEach((curSquadra) => {
-    // let {puntiFatti, falliSubiti} = curSquadra;
+    //assegnazione dei punti
     curSquadra.puntiFatti = generatorePuntiFalli(1,75);
+    //assegnazione dei falli
     curSquadra.falliSubiti = generatorePuntiFalli(1,40);
+    // stampa delle squadre
     console.log(curSquadra);
 });
+
+//Creazione con .map() di un nuovo array per poter inserire i dati voluti: nome, falliSubiti
+
+//creo la variabile squadreNuove e assegno la funzione map()
+//dentro squadra metto la squadra attuale che estrae map() e nelle chiavi inserisco il valore che si trova nel nome della chiave dell'oggetto squadreCalcio
+const squadreNuove = squadreCalcio.map(squadra => {
+    const nuovaSquadra = {
+        // squadreNuove.nome = squadreCalcio.nome
+        nome: squadra.nome,
+        // squadreNuove.falliSubiti = squadreCalcio.falliSubiti
+        falliSubiti: squadra.falliSubiti
+    };
+
+    //faccio il return di tutte le squadre con le nuove chiavi
+    return nuovaSquadra;
+});
+
+// stampo le squadre con template literal e destructuring
+squadreCalcio.forEach(curSquadraNuova => {
+    const {nome, falliSubiti} = curSquadraNuova;
+    console.log(`${nome}, ${falliSubiti}`);
+});
+
 
