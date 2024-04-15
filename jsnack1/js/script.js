f7c840bc518626cf335f4fac591459b0d465d2ee
Denis-Turbatu <turbatudenis81@gmail.com>
2024-04-15 14:39:34 +0200

    add: project init

---
+++ jsnack1/js/script.js
@@ -0,0 +1,23 @@
+const bici = [  //Dichiarazione array per le bici
+    { 
+        nome: "Graziella", 
+        peso: 15 
+    },
+    { 
+        nome: "Mountain Bike", 
+        peso: 22 
+    },
+    { 
+        nome: "Bici da corsa", 
+        peso: 10 
+    },
+    { 
+        nome: "Elettrica", 
+        peso: 24 
+    },
+    { 
+        nome: "BMX", 
+        peso: 12 
+    }
+];
+
